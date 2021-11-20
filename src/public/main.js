c1eb64106617d4b10d67aaee38e074626cedf737
AyTea14 <85985879+AyTea14@users.noreply.github.com>
2021-11-20 18:46:25 +0800

    Update main.js

--- src/public/main.js
+++ src/public/main.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("#input");
-const expandBtn = document.querySelector("#expand-btn");
+const shortenBtn = document.querySelector("#shorten-btn");
+const clipboardBtn = document.querySelector("#copy-btn");
 const result = document.querySelector("#result");
 
-expandBtn.addEventListener("click", () => {
+shortenBtn.addEventListener("click", () => {
     result.innerText = "Loading...";
     fetch(`/api/url/shorten`, {
         method: "POST",
@@ -15,6 +16,42 @@ expandBtn.addEventListener("click", () => {
             result.setAttribute("href", data.shortUrl);
         })
         .catch((error) => {
+            result.setAttribute("href", "");
             result.innerText = "Invalid URL";
         });
 });
+
+clipboardBtn.addEventListener("click", () => {
+    const url = result.href;
+    if (!url == "http://localhost:3000/" || !url == "https://shorten.aytea14.repl.co/") {
+        console.log(result.href);
+        copyTextToClipboard(url);
+    }
+});
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        document.execCommand("copy");
+    } catch (err) {}
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text);
+    alert("Copied url to the clipboard");
+}
