4fcc1c8909f561a89f0b35cfce3dde6779274770
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-01-21 22:48:52 +0800

    Updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const shortenURLs = require("../models/Url");
 const apiRoute = require("./url");
 const redirectRoute = require("./redirect");
 
@@ -11,39 +9,13 @@ const redirectRoute = require("./redirect");
  * @param {app} app
  */
 const routes = (app) => {
-    app.use(bodyParser.json());
+    app.use(express.json());
     app.use(cors());
     app.use(express.urlencoded({ extended: false }));
-    // app.use(express.static("src/public"));
     app.use(express.static("src/views"));
     app.set("view engine", "ejs");
     app.set("views", path.join(process.cwd(), "src/views"));
 
-    // app.get("/", async (req, res) => res.sendFile(path.join(`${__dirname}/../public/index.html`)));
-    app.get("/", async (req, res) => {
-        let shortURLs = await shortenURLs.find();
-        let clicks = shortURLs.reduce((previous, current) => previous + current.clicks, 0);
-
-        res.render("index", { shortURLs, clicks });
-    });
-    app.get("/:code/info", async (req, res) => {
-        let shortURL = await shortenURLs.findOne({ urlCode: req.params.code });
-        if (!shortURL) return res.render("error");
-        let longURL = `${shortURL.longUrl}`;
-        shortURL = `https://shrt.ml/${shortURL.urlCode}`;
-
-        res.render("info", { shortURL, longURL });
-    });
-    app.get("/:code/stats", async (req, res) => {
-        let shortURL = await shortenURLs.findOne({ urlCode: req.params.code });
-        if (!shortURL) return res.render("error");
-        let longURL = `${shortURL.longUrl}`;
-        let short = `https://shrt.ml/${shortURL.urlCode}`;
-        let date = new Date(shortURL.date);
-        let createdAt = new Intl.DateTimeFormat("en-GB", { dateStyle: "full", timeStyle: "long" }).format(date);
-
-        res.render("stats", { shortURL: short, clicked: shortURL.clicks, longURL, createdAt });
-    });
     app.use("/", redirectRoute);
     app.use("/api/url", apiRoute);
 };
--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Url = require("../models/Url");
 
+router.get("/", async (req, res) => {
+    try {
+        let shortURLs = await Url.find();
+        let clicks = shortURLs.reduce((previous, current) => previous + current.clicks, 0);
+
+        res.render("index", { shortURLs, clicks });
+    } catch (error) {}
+});
 router.get("/:code", async (req, res) => {
     try {
         const url = await Url.findOne({ urlCode: req.params.code });
-        if (!url) return res.status(404).send("No URL Found");
+        if (!url) return res.status(404).render("error");
 
         url.clicks++;
         url.save();
@@ -14,6 +22,29 @@ router.get("/:code", async (req, res) => {
     } catch (err) {
         res.status(500).send("Server Error");
     }
+});
+router.get("/:code/info", async (req, res) => {
+    let shortURL = await Url.findOne({ urlCode: req.params.code });
+    if (!shortURL) return res.render("error");
+    let longURL = `${shortURL.longUrl}`;
+    shortURL = `https://shrt.ml/${shortURL.urlCode}`;
+
+    res.render("info", { shortURL, longURL });
+});
+
+router.get("/:code/stats", async (req, res) => {
+    let shortURL = await Url.findOne({ urlCode: req.params.code });
+    if (!shortURL) return res.render("error");
+    let longURL = `${shortURL.longUrl}`;
+    let short = `https://shrt.ml/${shortURL.urlCode}`;
+    let date = new Date(shortURL.date);
+    let createdAt = new Intl.DateTimeFormat("en-GB", {
+        dateStyle: "full",
+        timeStyle: "long",
+        timeZone: "Europe/London",
+    }).format(date);
+
+    res.render("stats", { shortURL: short, clicked: shortURL.clicks, longURL, createdAt });
 });
 
 module.exports = router;
