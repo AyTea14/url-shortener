1adcafb0c085b477d207997f2b04e411044342f1
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-05-19 21:56:49 +0800

    Update url.js

--- src/routes/url.js
+++ src/routes/url.js
@@ -49,6 +49,7 @@ router.post("/shorten", async (req, res) => {
                 return res.json(url);
             } else if (url) {
                 url = await Url.findOne({ longUrl, custom_slug: false });
+                if (!url) url = new Url({ longUrl, urlCode: await chooseKey((s) => s), date: new Date(), custom_slug: false });
                 return res.json(url);
             } else {
                 url = new Url({ longUrl, urlCode, date: new Date(), custom_slug });
