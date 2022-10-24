8c4c17f96bbcab4464ceb24d76eef1a10a106c37
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-10-24 13:53:41 +0800

    Delete blocked.js

--- scripts/blocked.js
+++
@@ -1,6 +0,0 @@
-const { rm, cp } = require("fs/promises");
-
-(async () => {
-    await rm("dist/config", { recursive: true, force: true });
-    await cp("src/config", "dist/config", { recursive: true });
-})();
