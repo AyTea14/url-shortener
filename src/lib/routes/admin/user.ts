d661ba6d28406a6a7174de7e2e9d0b8c85e55156
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-10-25 18:24:24 +0800

    Update user.ts

--- src/lib/routes/admin/user.ts
+++ src/lib/routes/admin/user.ts
@@ -12,13 +12,12 @@ export async function me(fastify: FastifyInstance) {
             preHandler: fastify.auth([passAuth]),
             config: { rateLimit: { max: 1, timeWindow: "10s" } },
             handler: async function (req, reply) {
-                const { id: _id } = req.user!;
+                const { id } = req.user!;
                 const salt = encode(random(8));
                 const secret = encode(random(24));
-                const id = String(BigInt(_id) + BigInt(randomInt(5e8, 1e9)));
                 const token = `${encode(id)}.${salt}.${secret}`;
                 const hash = hashSecret(secret, salt);
-                await fastify.db.users.update({ where: { id: _id }, data: { token: hash } });
+                await fastify.db.users.update({ where: { id }, data: { token: hash } });
                 reply.type("application/json").send({ success: true, token });
             },
         })
