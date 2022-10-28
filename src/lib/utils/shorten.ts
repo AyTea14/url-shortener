d8448205e28dd96a2fe2610727128c58b4394021
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-10-28 17:38:05 +0800

    Update shorten.ts

--- src/lib/utils/shorten.ts
+++ src/lib/utils/shorten.ts
@@ -8,8 +8,8 @@ import { HttpCode } from "#lib/types";
 const MAX_SHORT_ID_GENERATION_ATTEMPTS = 10;
 
 export async function shorten(fastify: FastifyInstance, userId: string, url: string) {
-    let attempts = 0;
     let created: shortened | undefined = undefined;
+    let attempts = 0;
     let id: string;
 
     do {
@@ -26,9 +26,7 @@ export async function shorten(fastify: FastifyInstance, userId: string, url: str
             created = await fastify.db.shortened.create({ data: { code: encodedId, url, userId } });
         } catch (error) {
             if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
-            } else {
-                throw new Error(error as string);
-            }
+            } else throw new Error(error as string);
         }
     } while (!created);
 
