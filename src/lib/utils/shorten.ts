4dc140a2f4471af0916a3e0696496e35302c1432
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-10-28 15:43:58 +0800

    Update shorten.ts

--- src/lib/utils/shorten.ts
+++ src/lib/utils/shorten.ts
@@ -1,24 +1,36 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, shortened } from "@prisma/client";
 import { FastifyInstance } from "fastify";
 import { randomString } from "#lib/utils";
 import { config } from "#config";
+import { ExtendedError } from "#lib/exceptions";
+import { HttpCode } from "#lib/types";
 
 const MAX_SHORT_ID_GENERATION_ATTEMPTS = 10;
 
 export async function shorten(fastify: FastifyInstance, userId: string, url: string) {
+    let attempts = 0;
+    let created: shortened | undefined = undefined;
     let id: string;
 
-    id = randomString(config.shortLength);
-    const encodedId = Buffer.from(id, "ascii").toString("base64url");
+    do {
+        if (attempts++ > MAX_SHORT_ID_GENERATION_ATTEMPTS)
+            throw new ExtendedError(
+                `Couldn't generate a unique shortened ID in ${attempts} attempts`,
+                HttpCode["Internal Server Error"]
+            );
 
-    try {
-        await fastify.db.shortened.create({ data: { code: encodedId, url, userId } });
-    } catch (error) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
-        } else {
-            throw new Error(error as string);
+        id = randomString(config.shortLength);
+        const encodedId = Buffer.from(id, "ascii").toString("base64url");
+
+        try {
+            created = await fastify.db.shortened.create({ data: { code: encodedId, url, userId } });
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+            } else {
+                throw new Error(error as string);
+            }
         }
-    }
+    } while (!created);
 
     return id;
 }
