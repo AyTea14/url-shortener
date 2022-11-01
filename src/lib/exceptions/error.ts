6c07a9b56818d1a8e4a92558ac9b47496a2cd357
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-11-01 20:26:31 +0800

    Update error.ts

--- src/lib/exceptions/error.ts
+++ src/lib/exceptions/error.ts
@@ -1,12 +1,10 @@
-import { STATUS_CODES } from "http";
-
 export class ExtendedError extends Error {
     statusCode: number;
-    error: string;
 
     constructor(message: string, code: number) {
         super(message);
         this.statusCode = code;
-        this.error = STATUS_CODES[this.statusCode] as string;
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
